5d2a5c0bf6b08c0a5a612267342cdcb9098633b4
potatopotat0 <971327682@qq.com>
2021-01-11 15:58:20 +0800

    fix URL length limit, http detection and UTF-8 redirection page

--- index.php
+++ index.php
@@ -11,6 +11,22 @@
 			document.execCommand("copy");
 		}
 	</script>
+	<style>
+		@import url('https://fonts.googleapis.com/css?family=Noto+Sans+SC&amp;display=swap');
+		@import url('https://fonts.googleapis.com/css?family=Varela+Round&amp;display=swap');
+		@import url('https://intoyour.space/wp-content/fonts/KosugiMaru/style.css');
+		body {
+			font-family: 'Varela Round','Kosugi Maru','Noto Sans SC', 'SF Pro Text', 'Arial', 'PingFangSC', 'Microsoft Yahei', 'Microsoft Jhenghei', sans-serif;
+		}
+		a {color: black; text-decoration: none;}
+		a:link {color: black; text-decoration: none;}
+		a:visited {color: black; text-decoration: none;}
+		a:hover		{color: black; text-decoration: none;}
+		a:active {color: black; text-decoration: none;}
+		a.linkTxt:hover {color: rgba(135, 135, 135, .75) !important;}
+		a.linkImg {opacity: 1;}
+		a.linkImg:hover {opacity: .65;}
+	</style>
 </head>
 <body>
 	<form action = "https://ptt.pub/get.php" method = "get">
@@ -21,6 +37,10 @@
 			<button type="button" onclick="copyInput('urlInput')">Copy to clipboard</button>
 			<button type="button" onclick="window.location.replace('https://ptt.pub');">Return</button>
 		<?php endif ?>
-	</form>
+	</form><br>
+	© <a class = "linkTxt" href = "https://github.com/potatopotat0">potatopotat0</a>, Released under the <a class = "linkTxt" href = "https://github.com/potatopotat0/shortenURL/blob/main/LICENSE">MIT License</a>.<br>
+	<a class = "linkImg" href = "https://github.com/potatopotat0/shortenURL">
+		<img style = "transform: translate(0, 4px);" height = "24px" src = "https://bloghost-1254352323.cos.ap-hongkong.myqcloud.com/ImageUpload/GitHub_Logo.png" /><span style = "font-size: 20px;">REPOSITORY</span>
+	</a>
 </body>
 </html>
